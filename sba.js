6d6b8d18b27188d78cf92d349ee7d8bfd2ea8f7a
AbdouKKD <183544212+AbdouKKD@users.noreply.github.com>
2024-11-10 16:07:22 -0500

    Update sba.js

--- sba.js
+++ sba.js
@@ -1,1 +1,19 @@
 
+function promptExample(){
+    let dice = Math.floor(Math.random() *6) + 1
+    test =  document.getElementById('ice').innerText = prompt("Guess again")
+   
+
+
+    while(test!= dice){
+
+      if (Number(test) > dice ){
+        test = prompt("Number TOO HIGH")
+      } else if (Number(test) < dice) {
+        test =  prompt("Number is to low")
+      } else {alert("You didn't enter a number ?")
+        test = prompt("Guess AGAIN")
+      }
+       
+    }
+}
